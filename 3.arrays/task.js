2d3d661432ca95bab28b1bae0f1932b3f2197add
d.zhiborkin <danilkaololo67@gmail.com>
2023-03-25 12:51:58 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
+//Mission #1
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
+//Mission #2
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(user => user.gender === gender).map(item => item.age).reduce((acc, item, index, arr) => {
+        acc += item;
+        if (index === arr.length - 1) {
+            return acc / arr.length
+        }
+        return acc;
+    }, 0);
+    return result;
 }
