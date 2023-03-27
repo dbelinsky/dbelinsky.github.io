e22fac8ebf87d3b491cb0b3c53b1ba66e3a9540f
d.zhiborkin <danilkaololo67@gmail.com>
2023-03-28 01:43:09 +0300

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,49 @@
-﻿
+﻿function parseCount(value) {
+    if (isNaN(Number.parseFloat(value))) {
+        throw new Error('Невалидное значение')
+    }
+    return Number.parseFloat(value);
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if ((a + b) < c || (b + c) < a || (c + a) < b) {
+            throw error ('Треугольник с такими сторонами не существует');
+        }
+        this.a = a,
+        this.b = b,
+        this.c = c
+    }
+    get perimeter() {
+        return this.a + this.b + this.c
+    }
+    get area() {
+        const perimeter = this.a + this.b + this.c;
+        const p = perimeter / 2;
+        return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3))
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+                return 'Ошибка! Треугольник не существует'
+            },
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует'
+            }
+        };
+    }
+    return new Triangle(a, b, c);
+}
